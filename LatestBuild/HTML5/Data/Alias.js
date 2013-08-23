4f5dd80a1ff35ffadfff7e93765c8580559f06a6
bmerrill <bmerrill@bobmerrill.com>
2013-08-23 16:33:04 -0400

    5.7 Final (very few changes from 5.6 version)

---
+++ LatestBuild/HTML5/Data/Alias.js
@@ -0,0 +1,106 @@
+MadCap.Utilities.Xhr._FilePathToXmlStringMap.Add(
+	'Alias',
+	'<?xml version=\"1.0\" encoding=\"utf-8\"?>' +
+	'<CatapultAliasFile>' +
+	'    <Map Name=\"DEVGUIDECOVER\" Link=\"DevGuideCover.htm\" ResolvedId=\"1000\" />' +
+	'    <Map Name=\"INTRODUCTION\" Link=\"Introduction.html\" ResolvedId=\"1003\" />' +
+	'    <Map Name=\"THIRDPARTY\" Link=\"01-Preface/ThirdParty.htm\" ResolvedId=\"1005\" />' +
+	'    <Map Name=\"WARRANTY\" Link=\"01-Preface/warranty.html\" ResolvedId=\"1006\" />' +
+	'    <Map Name=\"ALARMPOINTFORMAT\" Link=\"02-CustomProbes/alarmpointformat.htm\" ResolvedId=\"1007\" />' +
+	'    <Map Name=\"ALARMPOINTNOTIFIERS\" Link=\"02-CustomProbes/alarmpointnotifiers.htm\" ResolvedId=\"1008\" />' +
+	'    <Map Name=\"ALARMPOINTS\" Link=\"02-CustomProbes/snmp-device-alarmpoints.htm\" ResolvedId=\"1009\" />' +
+	'    <Map Name=\"ANATOMY_OF_A_PROBE\" Link=\"02-CustomProbes/Anatomy_of_a_Probe.htm\" ResolvedId=\"1010\" />' +
+	'    <Map Name=\"APIACKNOWLEDGEMENTS\" Link=\"02-CustomProbes/APIAcknowledgements.htm\" ResolvedId=\"1011\" />' +
+	'    <Map Name=\"APIIMPORTINGEXPORTINGFILES\" Link=\"02-CustomProbes/APIImportingExportingFiles.htm\" ResolvedId=\"1012\" />' +
+	'    <Map Name=\"APIIMPORTINGEXPORTINGTABLES\" Link=\"02-CustomProbes/APIImportingExportingTables.htm\" ResolvedId=\"1013\" />' +
+	'    <Map Name=\"APIOVERVIEW\" Link=\"02-CustomProbes/APIOverview.htm\" ResolvedId=\"1014\" />' +
+	'    <Map Name=\"APISCRIPTINGEXAMPLES\" Link=\"02-CustomProbes/APIScriptingExamples.htm\" ResolvedId=\"1015\" />' +
+	'    <Map Name=\"BUILTINVARIABLEREFERENCE\" Link=\"02-CustomProbes/builtinvariablereference.htm\" ResolvedId=\"1017\" />' +
+	'    <Map Name=\"CMDLINEPROBEEXAMPLE\" Link=\"02-CustomProbes/cmdlineprobeexample.htm\" ResolvedId=\"1019\" />' +
+	'    <Map Name=\"COMMANDREF\" Link=\"02-CustomProbes/commandref.html\" ResolvedId=\"1020\" />' +
+	'    <Map Name=\"COMMAND_DISPLAY_SECTION\" Link=\"02-CustomProbes/command-display-section.htm\" ResolvedId=\"1021\" />' +
+	'    <Map Name=\"COMMAND_EXIT_SECTION\" Link=\"02-CustomProbes/command-exit-section.htm\" ResolvedId=\"1022\" />' +
+	'    <Map Name=\"COMMAND_LINE_SECTION\" Link=\"02-CustomProbes/command-line-section.htm\" ResolvedId=\"1023\" />' +
+	'    <Map Name=\"COMMANDLINEPROBES\" Link=\"02-CustomProbes/commandlineprobes.htm\" ResolvedId=\"1024\" />' +
+	'    <Map Name=\"COMMENTS\" Link=\"02-CustomProbes/comments.html\" ResolvedId=\"1025\" />' +
+	'    <Map Name=\"CUSTOM_PROBE_OVERVIEW\" Link=\"02-CustomProbes/Custom Probe Overview.htm\" ResolvedId=\"1026\" />' +
+	'    <Map Name=\"CUSTOMPROBES\" Link=\"02-CustomProbes/customprobes.html\" ResolvedId=\"1027\" />' +
+	'    <Map Name=\"CUSTOMSNMPPROBES\" Link=\"02-CustomProbes/customsnmpprobes.html\" ResolvedId=\"1028\" />' +
+	'    <Map Name=\"DARTWAREPROBEDATA\" Link=\"02-CustomProbes/dartwareprobedata.htm\" ResolvedId=\"1029\" />' +
+	'    <Map Name=\"DARTWAREMIB\" Link=\"02-CustomProbes/dartwaremib.html\" ResolvedId=\"1030\" />' +
+	'    <Map Name=\"DEBUGGING_WITH_SNMPWALK\" Link=\"02-CustomProbes/Debugging_with_SNMPWalk.htm\" ResolvedId=\"1031\" />' +
+	'    <Map Name=\"DESCRIPTION\" Link=\"02-CustomProbes/description.html\" ResolvedId=\"1032\" />' +
+	'    <Map Name=\"ENUMERATEDVALUESINSNMP\" Link=\"02-CustomProbes/EnumeratedValuesInSNMP.htm\" ResolvedId=\"1033\" />' +
+	'    <Map Name=\"ERRORS\" Link=\"02-CustomProbes/errors.html\" ResolvedId=\"1034\" />' +
+	'    <Map Name=\"FILENAMES\" Link=\"02-CustomProbes/filenames.html\" ResolvedId=\"1035\" />' +
+	'    <Map Name=\"HANDLINGTRAPS\" Link=\"02-CustomProbes/handlingtraps.htm\" ResolvedId=\"1036\" />' +
+	'    <Map Name=\"HEADER\" Link=\"02-CustomProbes/header.html\" ResolvedId=\"1037\" />' +
+	'    <Map Name=\"IMML___INTERMAPPER_MARKUP_LANGUAGE\" Link=\"02-CustomProbes/IMML - InterMapper Markup Language.htm\" ResolvedId=\"1038\" />' +
+	'    <Map Name=\"INSTALLING\" Link=\"02-CustomProbes/installing.html\" ResolvedId=\"1039\" />' +
+	'    <Map Name=\"LANGUAGE\" Link=\"02-CustomProbes/language.html\" ResolvedId=\"1040\" />' +
+	'    <Map Name=\"MODIFYING_BUILT_IN_PROBES\" Link=\"02-CustomProbes/Modifying Built-in Probes.htm\" ResolvedId=\"1041\" />' +
+	'    <Map Name=\"NAGIOS_PLUGIN_EXAMPLE\" Link=\"02-CustomProbes/Nagios Plugin Example.htm\" ResolvedId=\"1042\" />' +
+	'    <Map Name=\"NAGIOS\" Link=\"02-CustomProbes/nagios.htm\" ResolvedId=\"1043\" />' +
+	'    <Map Name=\"NOAA_PROBE\" Link=\"02-CustomProbes/NOAA_probe.htm\" ResolvedId=\"1044\" />' +
+	'    <Map Name=\"ONDEMANDTABLES\" Link=\"02-CustomProbes/OnDemandTables.htm\" ResolvedId=\"1045\" />' +
+	'    <Map Name=\"PARAMETERS\" Link=\"02-CustomProbes/parameters.html\" ResolvedId=\"1046\" />' +
+	'    <Map Name=\"PERSISTENTVARIABLES\" Link=\"02-CustomProbes/PersistentVariables.htm\" ResolvedId=\"1047\" />' +
+	'    <Map Name=\"PROBECALCULATIONS\" Link=\"02-CustomProbes/probecalculations.htm\" ResolvedId=\"1048\" />' +
+	'    <Map Name=\"PROBEPROPERTIES\" Link=\"02-CustomProbes/probeproperties.htm\" ResolvedId=\"1049\" />' +
+	'    <Map Name=\"PROBESTATUSWINDOWS\" Link=\"02-CustomProbes/probestatuswindows.html\" ResolvedId=\"1050\" />' +
+	'    <Map Name=\"PROBE_TOOL_SECTION\" Link=\"02-CustomProbes/Probe_Tool_Section.htm\" ResolvedId=\"1051\" />' +
+	'    <Map Name=\"PROBE_VARIABLES\" Link=\"02-CustomProbes/snmp-device-variables.htm\" ResolvedId=\"1052\" />' +
+	'    <Map Name=\"PYTHONPLUGINS\" Link=\"02-CustomProbes/PythonPlugins.htm\" ResolvedId=\"1053\" />' +
+	'    <Map Name=\"RECORDINGDATA\" Link=\"02-CustomProbes/recordingdata.htm\" ResolvedId=\"1054\" />' +
+	'    <Map Name=\"SHARING_CUSTOM_PROBES\" Link=\"02-CustomProbes/Sharing_Custom_Probes.htm\" ResolvedId=\"1055\" />' +
+	'    <Map Name=\"SNMP_DEVICE_DISPLAY_TRAPS\" Link=\"02-CustomProbes/snmp-device-display-traps.htm\" ResolvedId=\"1056\" />' +
+	'    <Map Name=\"SNMP_DEVICE_VARIABLES_TRAPS\" Link=\"02-CustomProbes/snmp-device-variables-traps.htm\" ResolvedId=\"1057\" />' +
+	'    <Map Name=\"SNMPDEVICEDISPLAYSECTION\" Link=\"02-CustomProbes/snmp-device-display.htm\" ResolvedId=\"1058\" />' +
+	'    <Map Name=\"SNMPOIDS\" Link=\"02-CustomProbes/snmpoids.htm\" ResolvedId=\"1059\" />' +
+	'    <Map Name=\"SNMPWALK_O\" Link=\"02-CustomProbes/snmpwalk_o.htm\" ResolvedId=\"1060\" />' +
+	'    <Map Name=\"TCPEXAMPLE\" Link=\"02-CustomProbes/tcpexample.html\" ResolvedId=\"1061\" />' +
+	'    <Map Name=\"TCPSCRIPTOUTPUTSECTION\" Link=\"02-CustomProbes/tcpscriptoutputsection.htm\" ResolvedId=\"1062\" />' +
+	'    <Map Name=\"TCP_PROBES\" Link=\"02-CustomProbes/TCP_Probes.htm\" ResolvedId=\"1063\" />' +
+	'    <Map Name=\"THRESHOLDS\" Link=\"02-CustomProbes/Thresholds.htm\" ResolvedId=\"1064\" />' +
+	'    <Map Name=\"TIMING\" Link=\"02-CustomProbes/timing.html\" ResolvedId=\"1065\" />' +
+	'    <Map Name=\"TRAPEXAMPLE\" Link=\"02-CustomProbes/trapexample.htm\" ResolvedId=\"1066\" />' +
+	'    <Map Name=\"TRAP_VIEWING_AND_LOGGING\" Link=\"02-CustomProbes/trap_viewing_and_logging.htm\" ResolvedId=\"1067\" />' +
+	'    <Map Name=\"TROUBLESHOOTINGPROBES\" Link=\"02-CustomProbes/TroubleshootingProbes.htm\" ResolvedId=\"1068\" />' +
+	'    <Map Name=\"USING_THE_COMMAND_LINE_INTERFACE\" Link=\"02-CustomProbes/Using_the_Command_Line_Interface.htm\" ResolvedId=\"1069\" />' +
+	'    <Map Name=\"CHARTS\" Link=\"03-CustomWebPages/charts.html\" ResolvedId=\"1070\" />' +
+	'    <Map Name=\"CUSTOMIZINGWEBPAGES\" Link=\"03-CustomWebPages/customizingwebpages.html\" ResolvedId=\"1071\" />' +
+	'    <Map Name=\"DIRECTIVES\" Link=\"03-CustomWebPages/directives.html\" ResolvedId=\"1072\" />' +
+	'    <Map Name=\"FOLDERS\" Link=\"03-CustomWebPages/folders.html\" ResolvedId=\"1073\" />' +
+	'    <Map Name=\"MACROS\" Link=\"03-CustomWebPages/macros.htm\" ResolvedId=\"1074\" />' +
+	'    <Map Name=\"MIME_TYPES\" Link=\"03-CustomWebPages/Mime_Types.htm\" ResolvedId=\"1075\" />' +
+	'    <Map Name=\"QUOTEDLINKS\" Link=\"03-CustomWebPages/quotedlinks.html\" ResolvedId=\"1076\" />' +
+	'    <Map Name=\"TARGETFILES\" Link=\"03-CustomWebPages/targetfiles.html\" ResolvedId=\"1077\" />' +
+	'    <Map Name=\"TEMPLATEFILES\" Link=\"03-CustomWebPages/templatefiles.html\" ResolvedId=\"1078\" />' +
+	'    <Map Name=\"CREATING_SQL_QUERIES\" Link=\"04-CustomReports/Creating_SQL_Queries.htm\" ResolvedId=\"1079\" />' +
+	'    <Map Name=\"DATABASESCHEMAS\" Link=\"04-CustomReports/DatabaseSchemas.htm\" ResolvedId=\"1080\" />' +
+	'    <Map Name=\"IMDBOVERVIEW\" Link=\"04-CustomReports/IMDBOverview.htm\" ResolvedId=\"1081\" />' +
+	'    <Map Name=\"WARRANTY1\" Link=\"01-Preface/warranty.html\" ResolvedId=\"1082\" />' +
+	'    <Map Name=\"COMMANDREF1\" Link=\"02-CustomProbes/commandref.html\" ResolvedId=\"1083\" />' +
+	'    <Map Name=\"COMMENTS1\" Link=\"02-CustomProbes/comments.html\" ResolvedId=\"1084\" />' +
+	'    <Map Name=\"CUSTOMPROBES1\" Link=\"02-CustomProbes/customprobes.html\" ResolvedId=\"1085\" />' +
+	'    <Map Name=\"CUSTOMSNMPPROBES1\" Link=\"02-CustomProbes/customsnmpprobes.html\" ResolvedId=\"1086\" />' +
+	'    <Map Name=\"DARTWAREMIB1\" Link=\"02-CustomProbes/dartwaremib.html\" ResolvedId=\"1087\" />' +
+	'    <Map Name=\"DESCRIPTION1\" Link=\"02-CustomProbes/description.html\" ResolvedId=\"1088\" />' +
+	'    <Map Name=\"ERRORS1\" Link=\"02-CustomProbes/errors.html\" ResolvedId=\"1089\" />' +
+	'    <Map Name=\"FILENAMES1\" Link=\"02-CustomProbes/filenames.html\" ResolvedId=\"1090\" />' +
+	'    <Map Name=\"HEADER1\" Link=\"02-CustomProbes/header.html\" ResolvedId=\"1091\" />' +
+	'    <Map Name=\"INSTALLING1\" Link=\"02-CustomProbes/installing.html\" ResolvedId=\"1092\" />' +
+	'    <Map Name=\"LANGUAGE1\" Link=\"02-CustomProbes/language.html\" ResolvedId=\"1093\" />' +
+	'    <Map Name=\"PARAMETERS1\" Link=\"02-CustomProbes/parameters.html\" ResolvedId=\"1094\" />' +
+	'    <Map Name=\"PROBESTATUSWINDOWS1\" Link=\"02-CustomProbes/probestatuswindows.html\" ResolvedId=\"1095\" />' +
+	'    <Map Name=\"TCPEXAMPLE1\" Link=\"02-CustomProbes/tcpexample.html\" ResolvedId=\"1096\" />' +
+	'    <Map Name=\"TIMING1\" Link=\"02-CustomProbes/timing.html\" ResolvedId=\"1097\" />' +
+	'    <Map Name=\"CHARTS1\" Link=\"03-CustomWebPages/charts.html\" ResolvedId=\"1098\" />' +
+	'    <Map Name=\"CUSTOMIZINGWEBPAGES1\" Link=\"03-CustomWebPages/customizingwebpages.html\" ResolvedId=\"1099\" />' +
+	'    <Map Name=\"DIRECTIVES1\" Link=\"03-CustomWebPages/directives.html\" ResolvedId=\"1100\" />' +
+	'    <Map Name=\"FOLDERS1\" Link=\"03-CustomWebPages/folders.html\" ResolvedId=\"1101\" />' +
+	'    <Map Name=\"QUOTEDLINKS1\" Link=\"03-CustomWebPages/quotedlinks.html\" ResolvedId=\"1102\" />' +
+	'    <Map Name=\"TARGETFILES1\" Link=\"03-CustomWebPages/targetfiles.html\" ResolvedId=\"1103\" />' +
+	'    <Map Name=\"TEMPLATEFILES1\" Link=\"03-CustomWebPages/templatefiles.html\" ResolvedId=\"1104\" />' +
+	'</CatapultAliasFile>'
+);
---
+++ LatestBuild/HTML5/Resources/Scripts/require.config.js
@@ -0,0 +1,3 @@
+require.config({
+    urlArgs: 't=635128722130581833'
+});
